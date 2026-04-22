068c37c3185fea42e2e5615246dbc83f9381baff
samco <sc00278@my.westga.edu>
2026-04-21 22:19:30 -0400

    adding the dataset to project

--- Training.py
+++ Training.py
@@ -4,3 +4,5 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import sklearn as sk
 
+data = pd.read_csv("steam_games_2026_adj.csv")
+print(data)
--- main.py
+++ main.py
@@ -13,9 +13,15 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("waddahali/top-1000-steam-games-20242026")
+
+print("Path to dataset files:", path)
